a39c8e66594c2ef8fdd996b7a0b69ba33fb1c963
fb@enyosolutions.com <fb@enyosolutions.com>
2021-12-24 08:28:38 +0000

    feat: various fix on schemas, admin, and linting

--- src/services/ws/wsModel.js
+++ src/services/ws/wsModel.js
@@ -51,79 +51,79 @@ module.exports = (socket) => {
       name, type, force, fields, withSchema
     } = req.body;
     switch (req.method) {
-      case 'GET':
-      default:
-        if (!axel.sqldb) {
-          return cb('sqldb_not_ready');
-        }
-        // eslint-disable -next-line
-        let tables = await axel.sqldb.query('show tables');
-        tables = tables.map(t => Object.values(t)[0]);
-        try {
-          const models = Object.entries(axel.models).map(([modelName, modelDef]) => ({
-            name: modelName,
-            fields: Object.keys(modelDef.entity.attributes).map(idx => ({
-              name: idx,
-              ...modelDef.entity.attributes[idx],
-              type: modelDef.properties,
-            })),
-          }));
-          cb(null, {
-            body: {
-              models,
-              tables,
-            },
-          });
-        } catch (err) {
-          cb(err.message || err);
-        }
-
-        break;
-      case 'POST':
-
-        if (!name) {
-          return cb('missing_param_name');
-        }
-        if (!type) {
-          return cb('missing_param_type');
-        }
-        if (!fields || !fields.length) {
-          return cb('missing_fields');
-        }
-        if (!fields.some(f => f.primaryKey)) {
-          return cb('missing_primary_key');
-        }
-        try {
-          let types = [type];
-          if (withSchema) {
-            types.push('schema');
-            types = _.uniq(types);
+        case 'GET':
+        default:
+          if (!axel.sqldb) {
+            return cb('sqldb_not_ready');
           }
-          generateModel({
-            name,
-            types,
-            force,
-            fields,
-          });
-          cb(null, { body: 'OK' });
-        } catch (err) {
-          console.warn('[AXELMANAGER]', err);
-          cb(err.message || 'See terminal for more details');
-        }
-        break;
-      case 'DELETE':
-        const modelPath = `${process.cwd()}/src/api/models/sequelize/${_.upperFirst(name)}.js`;
-        const schemaPath = `${process.cwd()}/src/api/models/schema/${_.upperFirst(name)}.js`;
-        try {
-          fs.unlinkSync(modelPath);
-          fs.unlinkSync(schemaPath);
-          cb(null, 'OK');
-          process.kill(process.pid, 'SIGUSR2');
-        } catch (err) {
-          console.warn(err);
-          cb(err.message || err);
-        }
-        break;
+          // eslint-disable -next-line
+          let tables = await axel.sqldb.query('show tables');
+          tables = tables.map(t => Object.values(t)[0]);
+          try {
+            const models = Object.entries(axel.models).map(([modelName, modelDef]) => ({
+              name: modelName,
+              fields: Object.keys(modelDef.entity.attributes).map(idx => ({
+                name: idx,
+                ...modelDef.entity.attributes[idx],
+                type: modelDef.properties,
+              })),
+            }));
+            cb(null, {
+              body: {
+                models,
+                tables,
+              },
+            });
+          } catch (err) {
+            cb(err.message || err);
+          }
+
+          break;
+        case 'POST':
+
+          if (!name) {
+            return cb('missing_param_name');
+          }
+          if (!type) {
+            return cb('missing_param_type');
+          }
+          if (!fields || !fields.length) {
+            return cb('missing_fields');
+          }
+          if (!fields.some(f => f.primaryKey)) {
+            return cb('missing_primary_key');
+          }
+          try {
+            let types = [type];
+            if (withSchema) {
+              types.push('schema');
+              types = _.uniq(types);
+            }
+            generateModel({
+              name,
+              types,
+              force,
+              fields,
+            });
+            cb(null, { body: 'OK' });
+          } catch (err) {
+            console.warn('[AXELMANAGER]', err);
+            cb(err.message || 'See terminal for more details');
+          }
+          break;
+        case 'DELETE':
+          const modelPath = `${process.cwd()}/src/api/models/sequelize/${_.upperFirst(name)}.js`;
+          const schemaPath = `${process.cwd()}/src/api/models/schema/${_.upperFirst(name)}.js`;
+          try {
+            fs.unlinkSync(modelPath);
+            fs.unlinkSync(schemaPath);
+            cb(null, 'OK');
+            process.kill(process.pid, 'SIGUSR2');
+          } catch (err) {
+            console.warn(err);
+            cb(err.message || err);
+          }
+          break;
     }
   });
   socket.on('/axel-manager/models/add-fields', async (req = { method: 'PUT', query: {}, body: {} }, cb) => {
@@ -132,50 +132,50 @@ module.exports = (socket) => {
     }
 
     switch (req.method) {
-      default:
-        cb('Incorrect method used');
-        break;
-      case 'POST':
-        const {
-          withSchema, fields, sync
-        } = req.body;
-        if (!req.body || !req.body.model) {
-          return cb('Missing model name');
-        }
-        const modelName = `${req.body.model}`;
-
-        if (!fields || !fields.length) {
-          return cb('missing_fields');
-        }
-        try {
-          const modelPath = `${process.cwd()}/src/api/models/sequelize/${_.upperFirst(modelName)}.js`;
-          const model = requireWithoutCache(modelPath);
-          const schemaPath = `${process.cwd()}/src/api/models/schema/${_.upperFirst(modelName)}.js`;
-          const schema = requireWithoutCache(schemaPath);
-          fields.forEach((field) => {
-            const sequelizeField = cliFieldToSequelizeField(field);
-            model.entity.attributes[field.name] = {
-              ...sequelizeField,
-            };
-
-            if (withSchema) {
-              schema.schema.properties[field.name] = sequelizeFieldToSchemaField(field.name, sequelizeField);
-            }
-          });
-
-          catchSignal('SIGUSR2', 5);
-
-          serializeModel(modelName, model);
-          serializeSchema(modelName, schema);
-
-          if (sync) {
-            axel.models[model.identity].em.sync({ alter: true }, { logging: true });
+        default:
+          cb('Incorrect method used');
+          break;
+        case 'POST':
+          const {
+            withSchema, fields, sync
+          } = req.body;
+          if (!req.body || !req.body.model) {
+            return cb('Missing model name');
           }
-          cb(null, { body: 'OK' });
-        } catch (err) {
-          console.warn('[AXELMANAGER]', err);
-          cb(err.message || 'See terminal for more details');
-        }
+          const modelName = `${req.body.model}`;
+
+          if (!fields || !fields.length) {
+            return cb('missing_fields');
+          }
+          try {
+            const modelPath = `${process.cwd()}/src/api/models/sequelize/${_.upperFirst(modelName)}.js`;
+            const model = requireWithoutCache(modelPath);
+            const schemaPath = `${process.cwd()}/src/api/models/schema/${_.upperFirst(modelName)}.js`;
+            const schema = requireWithoutCache(schemaPath);
+            fields.forEach((field) => {
+              const sequelizeField = cliFieldToSequelizeField(field);
+              model.entity.attributes[field.name] = {
+                ...sequelizeField,
+              };
+
+              if (withSchema) {
+                schema.schema.properties[field.name] = sequelizeFieldToSchemaField(field.name, sequelizeField);
+              }
+            });
+
+            catchSignal('SIGUSR2', 5);
+
+            serializeModel(modelName, model);
+            serializeSchema(modelName, schema);
+
+            if (sync) {
+              axel.models[model.identity].em.sync({ alter: true }, { logging: true });
+            }
+            cb(null, { body: 'OK' });
+          } catch (err) {
+            console.warn('[AXELMANAGER]', err);
+            cb(err.message || 'See terminal for more details');
+          }
     }
   });
 
@@ -184,30 +184,30 @@ module.exports = (socket) => {
       cb = req;
     }
     switch (req.method) {
-      case 'POST':
-      default:
-        if (!req.body) {
-          return cb('missing_param_body');
-        }
-        // @ts-ignore
-        const { id, force, alter } = req.body;
-        if (!id) {
-          return cb('missing_param_body');
-        }
-        const em = id === '$ALL' ? axel.sqldb : axel.models[id] && axel.models[id].em;
-        if (!em) {
-          return cb(`error_while_accessing_model${JSON.stringify(id)}`);
-        }
-        console.log('[AXELMANAGER] syncing sql model', id, { force, alter });
-        em.sync({ force, alter }, { logging: true })
-          .then(() => {
-            cb(null, { body: 'OK' });
-          })
-          .catch((err) => {
-            console.warn('[AXELMANAGER]', err);
-            cb(err.message || 'See terminal for more details');
-          });
-        break;
+        case 'POST':
+        default:
+          if (!req.body) {
+            return cb('missing_param_body');
+          }
+          // @ts-ignore
+          const { id, force, alter } = req.body;
+          if (!id) {
+            return cb('missing_param_body');
+          }
+          const em = id === '$ALL' ? axel.sqldb : axel.models[id] && axel.models[id].em;
+          if (!em) {
+            return cb(`error_while_accessing_model${JSON.stringify(id)}`);
+          }
+          console.log('[AXELMANAGER] syncing sql model', id, { force, alter });
+          em.sync({ force, alter }, { logging: true })
+            .then(() => {
+              cb(null, { body: 'OK' });
+            })
+            .catch((err) => {
+              console.warn('[AXELMANAGER]', err);
+              cb(err.message || 'See terminal for more details');
+            });
+          break;
     }
   });
 
@@ -217,16 +217,16 @@ module.exports = (socket) => {
       cb = req;
     }
     switch (req.method) {
-      case 'GET':
-      default:
-        break;
-      case 'POST':
-        AxelAdmin.clearModelsInDb()
-          .then(() => {
-            cb();
-            process.kill(process.pid, 'SIGUSR2');
-          })
-          .catch(cb);
+        case 'GET':
+        default:
+          break;
+        case 'POST':
+          AxelAdmin.clearModelsInDb()
+            .then(() => {
+              cb();
+              process.kill(process.pid, 'SIGUSR2');
+            })
+            .catch(cb);
     }
   });
 
@@ -236,14 +236,14 @@ module.exports = (socket) => {
       cb = req;
     }
     switch (req.method) {
-      case 'GET':
-        AxelAdmin.serveModels().then((models) => {
-          cb(null, { body: models });
-        })
-          .catch(err => cb(err.message));
-        break;
-      default:
-        break;
+        case 'GET':
+          AxelAdmin.serveModels().then((models) => {
+            cb(null, { body: models });
+          })
+            .catch(err => cb(err.message));
+          break;
+        default:
+          break;
     }
   });
 
@@ -259,47 +259,47 @@ module.exports = (socket) => {
       return cb('missing_param_model_name');
     }
     switch (req.method) {
-      case 'PUT':
-        getMergedModel(req.body.modelName).then((model) => {
-          if (!model) {
-            return cb('model_not_found');
-          }
-          const {
-            identity,
-            primaryKeyField,
-            primaryKey,
-            displayField,
-            schema,
-          } = model;
-          catchSignal('SIGUSR2', 5);
-          const modelToSave = {
-            identity,
-            primaryKeyField,
-            primaryKey,
-            displayField,
-            schema,
-            admin: {
-              ...model
+        case 'PUT':
+          getMergedModel(req.body.modelName).then((model) => {
+            if (!model) {
+              return cb('model_not_found');
             }
-          };
+            const {
+              identity,
+              primaryKeyField,
+              primaryKey,
+              displayField,
+              schema,
+            } = model;
+            catchSignal('SIGUSR2', 5);
+            const modelToSave = {
+              identity,
+              primaryKeyField,
+              primaryKey,
+              displayField,
+              schema,
+              admin: {
+                ...model
+              }
+            };
 
 
-          [
-            'identity',
-            'schema',
-            'primaryKeyField',
-            'primaryKey',
-            'displayField',
-            'admin',
-          ].forEach(f => delete modelToSave.admin[f]);
+            [
+              'identity',
+              'schema',
+              'primaryKeyField',
+              'primaryKey',
+              'displayField',
+              'admin',
+            ].forEach(f => delete modelToSave.admin[f]);
 
-          serializeSchema(req.body.modelName, modelToSave);
-          cb(null, { body: model });
-        })
-          .catch(err => cb(err.message));
-        break;
-      default:
-        break;
+            serializeSchema(req.body.modelName, modelToSave);
+            cb(null, { body: model });
+          })
+            .catch(err => cb(err.message));
+          break;
+        default:
+          break;
     }
   });
 };
